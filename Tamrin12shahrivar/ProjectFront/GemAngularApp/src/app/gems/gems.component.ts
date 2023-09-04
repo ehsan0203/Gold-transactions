70fc51233b5c8681064ef1bebe38cbf22bdb4974
ehsan <60423941+ehsan0203@users.noreply.github.com>
2023-09-04 11:31:11 +0430

    Create Front Angular Project Gem with Bootstrap

---
+++ Tamrin12shahrivar/ProjectFront/GemAngularApp/src/app/gems/gems.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gems',
+  templateUrl: './gems.component.html',
+  styleUrls: ['./gems.component.css']
+})
+export class GemsComponent {
+
+}
